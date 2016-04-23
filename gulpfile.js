8fda7976e965eeead94e1d688828039cc25758d0
Guillaume Clochard <guillaume.clochard@etu.univ-nantes.fr>
2016-04-23 06:45:30 +0200

    Add jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 (() => {
     'use strict';
 
-    let gulp = require('gulp'),
-        sass = require('gulp-sass');
+    let gulp        = require('gulp'),
+        sass        = require('gulp-sass'),
+        jshint      = require('gulp-jshint'),
+        stylish     = require('jshint-stylish');
 
     let paths = {
         sass: {
             src  : `./styles/sass/**/*.scss`,
             dest : `./styles/css`
+        },
+        lint: {
+            src : `./gulpfile.js`
         }
     };
 
@@ -25,6 +30,23 @@
         gulp.watch(paths.sass.src, ['sass']);
     });
 
-    gulp.task('default', ['sass:watch']);
+    /**
+     * jshint
+     */
+
+    gulp.task('lint', function() {
+        return gulp.src(paths.lint.src)
+            .pipe(jshint())
+            .pipe(jshint.reporter(stylish))
+            .pipe(jshint.reporter('fail'));
+    });
+
+    gulp.task('lint:watch', () => {
+        gulp.watch(paths.lint.src, ['lint']);
+    });
+
+    gulp.task('check', ['lint']);
+    gulp.task('build', ['sass']);
+    gulp.task('default', ['check', 'build', 'sass:watch', 'lint:watch']);
 
 }());
